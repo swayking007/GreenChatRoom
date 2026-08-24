fa8c9d8785a7482aa99b1e9901f851f6415d677e
Swayam <officialswayamsahu01@gmail.com>
2026-08-24 20:08:49 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(__dirname));
+
+io.on('connection', (socket) => {
+    console.log("User connected:", socket.id);
+
+    // Join room
+    socket.on('joinRoom', ({ username, room }) => {
+        socket.join(room);
+        socket.username = username;
+        socket.room = room;
+
+        console.log(`${username} joined ${room}`);
+
+        // Notify others
+        socket.to(room).emit('chat message', {
+            user: "System",
+            text: `${username} joined the room`
+        });
+    });
+
+    // Chat message
+    socket.on('chat message', (msg) => {
+        io.to(socket.room).emit('chat message', {
+            user: socket.username,
+            text: msg
+        });
+    });
+
+    // Disconnect
+    socket.on('disconnect', () => {
+        if (socket.username && socket.room) {
+            io.to(socket.room).emit('chat message', {
+                user: "System",
+                text: `${socket.username} left the room`
+            });
+        }
+        console.log("User disconnected:", socket.id);
+    });
+});
+
+server.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
